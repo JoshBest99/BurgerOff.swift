45669a55c9dce3a90a8883e0f1c3fdac3b1dc2cd
Josh <Joshuabest99@hotmail.com>
2019-04-02 13:39:26 +0100

    Initial Commit

---
+++ BurgerOff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BurgerOff
+//
+//  Created by Joshua Best on 02/04/2019.
+//  Copyright © 2019 JoshInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
